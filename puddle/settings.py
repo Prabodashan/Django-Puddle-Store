392eef56a6bdf41b91aaaa1c853cb34d4afa00d5
Prabodashan <prabodashan14@gmail.com>
2023-03-31 16:10:08 +0530

    added message page and function

--- puddle/settings.py
+++ puddle/settings.py
@@ -42,9 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'conversation',
     'core',
-    'item',
     'dashboard',
+    'item',
 ]
 
 MIDDLEWARE = [
