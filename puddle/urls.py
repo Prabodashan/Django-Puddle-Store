32001744f4a5dac091a49ba435bcba09d1b1b5bd
Prabodashan <prabodashan14@gmail.com>
2023-03-31 13:47:16 +0530

    added items to front page

--- puddle/urls.py
+++ puddle/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -7,4 +9,4 @@ urlpatterns = [
     path('contact/', contact, name='contact'),
     path('', index, name='index'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
